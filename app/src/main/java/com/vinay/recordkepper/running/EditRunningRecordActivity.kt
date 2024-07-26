32556a85ba9b98a29981876f0cd130362f2aac13
vinay <raparthivinay99@gmail.com>
2024-07-26 10:10:52 +0530

    update

--- app/src/main/java/com/vinay/recordkepper/running/EditRunningRecordActivity.kt
+++ app/src/main/java/com/vinay/recordkepper/running/EditRunningRecordActivity.kt
@@ -1,7 +1,10 @@
 package com.vinay.recordkepper.running
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
 import com.vinay.recordkepper.databinding.ActivityEditRunningRecordBinding
 
 class EditRunningRecordActivity : AppCompatActivity() {
@@ -13,5 +16,23 @@ class EditRunningRecordActivity : AppCompatActivity() {
         setContentView(binding.root)
         val distance = intent.getStringExtra("Distance")
         title = "$distance Record"
+
+        val applicationPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        applicationPreferences.edit {
+            putString("Some application data", "Application Preference value")
+        }
+
+        val activityPreferences = getPreferences(Context.MODE_PRIVATE)
+        activityPreferences.edit {
+            putInt("Some activity data",15)
+        }
+
+        val fileNamePreferences = getSharedPreferences("some shared preferences name",Context.MODE_PRIVATE)
+
+        fileNamePreferences.edit {
+            putBoolean("some preference file name data", false)
+        }
+
     }
 }
